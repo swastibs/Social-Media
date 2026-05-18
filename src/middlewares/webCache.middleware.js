7025ced51b48633678bb85c28027e9e285d26177
swastisunder <swastisunderbadatya@team.bigscal.com>
2026-05-18 11:09:36 +0530

    feat: Implement web caching middleware and integrate it into web routes

---
+++ src/middlewares/webCache.middleware.js
@@ -0,0 +1,47 @@
+const { getCache, setCache, generateCacheKey } = require("../utils/cache");
+
+const WEB_PREFIX = "web:";
+const DEFAULT_TTL = 60 * 60;
+
+module.exports = (ttl = DEFAULT_TTL) => {
+  return async (req, res, next) => {
+    if (req.method !== "GET") return next();
+
+    const userId = req.user?.id || "guest";
+    let originalUrl = req.originalUrl;
+    if (originalUrl.endsWith("/")) originalUrl = originalUrl.slice(0, -1);
+    const keyBase = `${originalUrl}|user:${userId}`;
+    const cacheKey = `${WEB_PREFIX}${generateCacheKey({ originalUrl: keyBase })}`;
+
+    try {
+      const cachedHtml = await getCache(cacheKey);
+      if (cachedHtml) {
+        res.setHeader("X-Cache", "HIT");
+        return res.send(cachedHtml);
+      }
+
+      res.setHeader("X-Cache", "MISS");
+
+      const originalSend = res.send;
+      let capturedHtml = null;
+
+      res.send = function (body) {
+        if (typeof body === "string" && res.statusCode === 200) {
+          capturedHtml = body;
+        }
+        return originalSend.call(this, body);
+      };
+
+      res.once("finish", () => {
+        if (capturedHtml && res.statusCode === 200) {
+          setCache(cacheKey, capturedHtml, ttl).catch(() => {});
+        }
+        res.send = originalSend;
+      });
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
