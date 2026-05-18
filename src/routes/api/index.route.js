17553012e8ee4e11a725ce93e789aa494695ed24
swastisunder <swastisunderbadatya@team.bigscal.com>
2026-05-18 12:28:47 +0530

    feat: Implement rate limiting middleware across API and web routes

--- src/routes/api/index.route.js
+++ src/routes/api/index.route.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 
+const rateLimiter = require("../../middlewares/rateLimiter.middleware");
 const authRouter = require("./auth.route");
 const userRouter = require("./user.route");
 const postRouter = require("./post.route");
 const commentRouter = require("./comment.route");
 const activityRouter = require("./activity.route");
+
+router.use(rateLimiter(60, 200, "api-global"));
+
+// API Routes
 
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
--- src/routes/api/user.route.js
+++ src/routes/api/user.route.js
@@ -1,22 +1,12 @@
 const userRouter = require("express").Router();
+const { validate } = require("express-validation");
 
 const { authenticate } = require("../../middlewares/auth.middleware");
 const { authorize } = require("../../middlewares/authorize.middleware");
 const { ROLES } = require("../../constant/role");
+
 const upload = require("../../middlewares/multer");
-const {
-  userIdParamSchema,
-  getAllUsersSchema,
-  updateUserActionSchema,
-  getAllPostsOfUserSchema,
-  getPostOfUserSchema,
-  getAllCommentsOfUserSchema,
-  getCommentOfUserSchema,
-  followUserSchema,
-  getFollowersSchema,
-  getFollowingSchema,
-  updateProfileSchema,
-} = require("../../validations/api/user.validation");
+const rateLimiter = require("../../middlewares/rateLimiter.middleware");
 
 const {
   getAllUsers,
@@ -32,103 +22,143 @@ const {
   getFollowing,
   updateProfile,
 } = require("../../controllers/api/user.controller");
-const { validate } = require("express-validation");
+
+const {
+  userIdParamSchema,
+  getAllUsersSchema,
+  updateUserActionSchema,
+  getAllPostsOfUserSchema,
+  getPostOfUserSchema,
+  getAllCommentsOfUserSchema,
+  getCommentOfUserSchema,
+  followUserSchema,
+  getFollowersSchema,
+  getFollowingSchema,
+  updateProfileSchema,
+} = require("../../validations/api/user.validation");
+
 const { cacheMiddleware } = require("../../middlewares/cache.middleware");
 const { invalidateCache } = require("../../middlewares/invalidate.middleware");
 
+// Authentication middleware for all routes
 userRouter.use(authenticate);
 
+// Get All Users
 userRouter.get(
   "/",
   authorize(ROLES.ADMIN, ROLES.USER),
+  rateLimiter(60, 100, "get-users"),
   validate(getAllUsersSchema),
   cacheMiddleware(),
   getAllUsers,
 );
 
+// Get Single User
 userRouter.get(
   "/:userId",
   authorize(ROLES.ADMIN, ROLES.USER),
+  rateLimiter(60, 100, "get-user"),
   validate(userIdParamSchema),
   cacheMiddleware(),
   getUser,
 );
 
+// Delete User
 userRouter.delete(
   "/:userId",
   authorize(ROLES.ADMIN),
+  rateLimiter(60, 10, "admin-delete"),
   validate(userIdParamSchema),
   invalidateCache(["cache:/api/users*", "cache:/api/activities*"]),
   deleteUser,
 );
 
+// Get All Posts Of User
 userRouter.get(
   "/:userId/posts",
   authorize(ROLES.ADMIN, ROLES.USER),
+  rateLimiter(60, 100, "get-user-posts"),
   validate(getAllPostsOfUserSchema),
   cacheMiddleware(),
   getAllPostsOfUser,
 );
 
+// Get Single Post Of User
 userRouter.get(
   "/:userId/posts/:postId",
   authorize(ROLES.ADMIN, ROLES.USER),
+  rateLimiter(60, 100, "get-user-post"),
   validate(getPostOfUserSchema),
   cacheMiddleware(),
   getPostOfUser,
 );
 
+// Get All Comments Of User
 userRouter.get(
   "/:userId/comments",
   authorize(ROLES.ADMIN, ROLES.USER),
+  rateLimiter(60, 100, "get-user-comments"),
   validate(getAllCommentsOfUserSchema),
   cacheMiddleware(),
   getAllCommentsOfUser,
 );
 
+// Get Single Comment Of User
 userRouter.get(
   "/:userId/comments/:commentId",
   authorize(ROLES.ADMIN, ROLES.USER),
+  rateLimiter(60, 100, "get-user-comment"),
   validate(getCommentOfUserSchema),
   cacheMiddleware(),
   getCommentOfUser,
 );
 
+// Follow / Unfollow User
 userRouter.put(
   "/:userId/follow",
   authorize(ROLES.USER),
+  rateLimiter(60, 30, "follow-user"),
   validate(followUserSchema),
   invalidateCache(["cache:/api/users*", "cache:/api/activities*"]),
   followUnfollowUser,
 );
 
+// Admin User Action
 userRouter.put(
   "/:userId/:action",
   authorize(ROLES.ADMIN),
+  rateLimiter(60, 20, "admin-action"),
   validate(updateUserActionSchema),
   invalidateCache(["cache:/api/users*", "cache:/api/activities*"]),
   updateUserAction,
 );
 
+// Update User Profile
 userRouter.put(
   "/profile",
   authorize(ROLES.USER),
+  rateLimiter(60, 10, "update-profile"),
   validate(updateProfileSchema),
   upload.single("profilePicture"),
+  invalidateCache(["cache:/api/users*", "cache:/api/activities*"]),
   updateProfile,
 );
 
+// Get Followers
 userRouter.get(
   "/:userId/followers",
   authorize(ROLES.ADMIN, ROLES.USER),
+  rateLimiter(60, 100, "get-followers"),
   validate(getFollowersSchema),
   cacheMiddleware(),
   getFollowers,
 );
 
+// Get Following
 userRouter.get(
   "/:userId/following",
   authorize(ROLES.ADMIN, ROLES.USER),
+  rateLimiter(60, 100, "get-following"),
   validate(getFollowingSchema),
   cacheMiddleware(),
   getFollowing,
